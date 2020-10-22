c68954d5b083e2e4e0e107f22eed48f4e7962d44
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-10-22 02:52:19 +0200

    added 10 to 2x conversion
    it works but the division has no lines yet

--- src/main/kotlin/com/psd_helper/backend/Conversions.kt
+++ src/main/kotlin/com/psd_helper/backend/Conversions.kt
@@ -1,7 +1,6 @@
 package com.psd_helper.backend
 
 import com.psd_helper.Constants
-import dev.federicocapece.drawzone.Drawable
 import dev.federicocapece.drawzone.Group
 import dev.federicocapece.drawzone.drawables.Text
 import kotlin.math.pow
@@ -36,26 +35,93 @@ class Conversions {
                 }
             }else{
                 //base 10 to 2x
-                return consecutiveDivisions(inputNum, inputBase, outputBase, canvas, preferredWidth)
+                return consecutiveDivisions(inputNum, outputBase, canvas, preferredWidth)
             }
         }
 
-        private fun consecutiveDivisions(inputNum: String, inputBase: Int, outputBase: Int, canvas: Group, preferredWidth: Double): String {
-            TODO("Not yet implemented")
+        private fun consecutiveDivisions(inputNum: String, outputBase: Int, canvas: Group, preferredWidth: Double): String {
+            var lastDivision = canvas
             var toDivide:Int = inputNum.toInt()
             //write number
-            var numberDrawable = Text(inputNum)
-            canvas.add(numberDrawable)
+            var toDivideDrawable = Text(toDivide.toString())
+            lastDivision.add(toDivideDrawable)
 
-            var divisionResult = -1
-            while(divisionResult != 0){
+            var remainders = arrayListOf<Int>()
+            while(toDivide != 0){
+                remainders.add(toDivide % outputBase)
+                toDivide /= outputBase
+
+                lastDivision = division(lastDivision.last() as Text, outputBase, lastDivision)
 
             }
-            //write division
+
+
+            return remainders.map { it.toString(outputBase) }.reversed().joinToString("")
         }
 
-        private fun division(text: Text, dividend:Int, canvas : Group) : Text{
-            TODO("Not yet implemented")
+        private fun division(toDivideText: Text, dividend:Int, canvas : Group) : Group{
+            //creating an output division group
+            val output = canvas.group(toDivideText.x, toDivideText.y)
+            canvas.add(output)
+
+            //reassigning the text to the new division group
+            canvas.remove(toDivideText)
+            output.add(toDivideText)
+            toDivideText.y = .0; toDivideText.x = .0
+
+            //adding the dividend
+            val dividendText = Text(dividend.toString(), toDivideText.right + 10)
+            output.add(dividendText)
+
+            //starting the effective division procedure
+            var boughtDownNumbers = 0
+            var remainder = 0
+            var resultString = ""
+            while(remainder >= dividend || boughtDownNumbers < toDivideText.text.length){
+                //reverse multiply and subtract if the remainder is bigger than the dividend
+                if(remainder >= dividend){
+                    val multiple = (remainder / dividend)
+                    val toSubtract = multiple * dividend
+                    remainder -= toSubtract
+                    resultString += multiple
+
+                    val toSubtractString =
+                            if(output.count()!=2)
+                                spaceFill(toSubtract.toString(), (output.last() as Text).text.length)
+                            else
+                                spaceFill(toSubtract.toString(), remainder.toString().length)
+                    val remainderString = spaceFill(remainder.toString(), (output.last() as Text).text.length)
+
+                    output.add(Text(toSubtractString, y = (output.last() as Text).bottom))
+                    output.add(Text(remainderString, y = (output.last() as Text).bottom))
+                }
+
+                //bought down a number to the remainder if you need to
+                if(remainder<dividend && boughtDownNumbers < toDivideText.text.length){
+                    val bringDown = toDivideText.text[boughtDownNumbers]
+                    remainder = (remainder.toString() + bringDown).toInt()
+                    if(output.count()!=2) //2 = input and base
+                        (output.last() as Text).text += bringDown
+                    boughtDownNumbers++
+                }
+            }
+
+            if(resultString.isEmpty())
+                resultString = "0"
+
+            //adding the result
+            output.add(Text(resultString, x = dividendText.x, y = dividendText.bottom))
+
+            return output
+        }
+
+        private fun spaceFill(string: String, length: Int): String = charFill(string,length, ' ')
+
+        private fun charFill(string: String, length: Int, char: Char): String {
+            var toFill = length - string.length
+            toFill = if(toFill<0) 0 else toFill
+
+            return char.toString().repeat(toFill) + string
         }
 
         private fun quick2To2Conversion(inputNum: String, inputBase: Int, outputBase: Int, canvas: Group, preferredWidth: Double): String {
